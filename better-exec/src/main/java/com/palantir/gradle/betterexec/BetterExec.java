fe9801d0c5682553addd8e715fd6188a74891237
Finlay Williams <finlayw@palantir.com>
2025-10-13 09:58:42 +0100

    make api

--- better-exec/src/main/java/com/palantir/gradle/betterexec/BetterExec.java
+++ better-exec/src/main/java/com/palantir/gradle/betterexec/BetterExec.java
@@ -23,10 +23,10 @@ import java.util.stream.IntStream;
 import javax.inject.Inject;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.RegularFile;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.Internal;
-import org.gradle.api.tasks.Nested;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.workers.WorkQueue;
 import org.gradle.workers.WorkerExecutor;
@@ -38,25 +38,21 @@ public abstract class BetterExec extends DefaultTask implements BetterExecCommon
     @Inject
     protected abstract WorkerExecutor getWorkerExecutor();
 
-    @Nested
-    protected abstract CircleCiArtifacts getCircleCiArtifacts();
-
-    @Nested
-    protected abstract EnvironmentVariables getEnvVarsFromUtils();
-
     @Internal
-    protected abstract Property<ArtifactLocation> getArtifactLocation();
+    protected abstract Property<String> getCircleLink();
+
+    @Inject
+    protected abstract ObjectFactory getObjectFactory();
 
     public BetterExec() {
-        getArtifactLocation().set(findAvailableLocation(getProject().getName() + "." + getName()));
-        getArtifactLocation().finalizeValueOnRead();
+        Provider<ArtifactLocation> location = findAvailableLocation(getProject().getName() + "." + getName());
+
+        getCircleLink().set(location.map(ArtifactLocation::circleLink).orElse(""));
 
         getWorkingDir().set(".");
 
         getCircleLogFilePath()
-                .fileProvider(getArtifactLocation()
-                        .map(ArtifactLocation::physicalPath)
-                        .map(RegularFile::getAsFile));
+                .fileProvider(location.map(ArtifactLocation::physicalPath).map(RegularFile::getAsFile));
 
         getShowRealTimeLogs().set(isOnCi().map(isOnCi -> !isOnCi));
         getCheckExitStatus().set(true);
@@ -81,8 +77,7 @@ public abstract class BetterExec extends DefaultTask implements BetterExecCommon
 
             params.getRetryWhen().set(retryWhen);
             params.getIsOnCi().set(isOnCi());
-            params.getCircleArtifactsUrlLocation()
-                    .set(getArtifactLocation().map(ArtifactLocation::circleLink).orElse(""));
+            params.getCircleArtifactsUrlLocation().set(getCircleLink());
         });
     }
 
@@ -109,14 +104,17 @@ public abstract class BetterExec extends DefaultTask implements BetterExecCommon
     }
 
     private Provider<Boolean> isOnCi() {
-        return getEnvVarsFromUtils()
+        return getObjectFactory()
+                .newInstance(EnvironmentVariables.class)
                 .envVarOrFromTestingProperty("CI")
                 .map(_value -> true)
                 .orElse(false);
     }
 
     private Provider<ArtifactLocation> findAvailableLocation(String baseName) {
-        return getCircleCiArtifacts().resolveArtifactLocation(baseName + ".log").map(location -> {
+        CircleCiArtifacts artifacts = getObjectFactory().newInstance(CircleCiArtifacts.class);
+
+        return artifacts.resolveArtifactLocation(baseName + ".log").map(location -> {
             if (!location.physicalPath().getAsFile().exists()) {
                 return location;
             }
@@ -124,9 +122,7 @@ public abstract class BetterExec extends DefaultTask implements BetterExecCommon
             return IntStream.iterate(2, i -> i + 1)
                     .mapToObj(i -> {
                         String fileName = baseName + "." + i + ".log";
-                        return getCircleCiArtifacts()
-                                .resolveArtifactLocation(fileName)
-                                .get();
+                        return artifacts.resolveArtifactLocation(fileName).get();
                     })
                     .filter(loc -> !loc.physicalPath().getAsFile().exists())
                     .findFirst()
