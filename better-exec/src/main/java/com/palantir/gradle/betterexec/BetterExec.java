decae156de6365113f96e87d08048ed604176179
FinlayRJW <88376465+FinlayRJW@users.noreply.github.com>
2026-03-16 10:16:47 +0000

    Make `circleci-artifacts` and `environment-variables` implementation dependencies (#292)
    `circleci-artifacts` and `environment-variables` are `implementation` dependencies instead of `api`.
    
    All CircleCI artifact and environment variable types are moved into a package-private `BetterExecInternals` helper class, so `BetterExec.class` bytecode no longer directly references them. This means the JVM can load `BetterExec` without those types on the compile classpath, and `validatePlugins` succeeds in consumer plugins that extend `BetterExec`.

--- better-exec/src/main/java/com/palantir/gradle/betterexec/BetterExec.java
+++ better-exec/src/main/java/com/palantir/gradle/betterexec/BetterExec.java
@@ -15,18 +15,10 @@
  */
 package com.palantir.gradle.betterexec;
 
-import com.palantir.gradle.utils.circleciartifacts.ArtifactLocation;
-import com.palantir.gradle.utils.circleciartifacts.CircleCiArtifacts;
-import com.palantir.gradle.utils.environmentvariables.EnvironmentVariables;
+import com.google.errorprone.annotations.RestrictedApi;
 import groovy.lang.Closure;
-import java.util.stream.IntStream;
 import javax.inject.Inject;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.file.RegularFile;
-import org.gradle.api.provider.Property;
-import org.gradle.api.provider.Provider;
-import org.gradle.api.tasks.Internal;
-import org.gradle.api.tasks.Nested;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.workers.WorkQueue;
 import org.gradle.workers.WorkerExecutor;
@@ -34,38 +26,30 @@ import org.gradle.workers.WorkerExecutor;
 public abstract class BetterExec extends DefaultTask implements BetterExecCommon {
 
     private final SerializableOrSpec<String> retryWhen = SerializableOrSpec.empty();
+    private final BetterExecInternals internals;
 
     @Inject
-    protected abstract WorkerExecutor getWorkerExecutor();
-
-    @Nested
-    protected abstract CircleCiArtifacts getCircleCiArtifacts();
-
-    @Nested
-    protected abstract EnvironmentVariables getEnvVarsFromUtils();
-
-    @Internal
-    protected abstract Property<ArtifactLocation> getArtifactLocation();
+    @RestrictedApi(
+            explanation = "Do not use — internal to BetterExec",
+            allowedOnPath = ".*/com/palantir/gradle/betterexec/.*")
+    protected abstract WorkerExecutor getBetterExecInternalWorkerExecutor();
 
     public BetterExec() {
-        getArtifactLocation().set(findAvailableLocation(getProject().getName() + "." + getName()));
-        getArtifactLocation().finalizeValueOnRead();
+        internals =
+                new BetterExecInternals(getProject().getObjects(), getProject().getName() + "." + getName());
 
         getWorkingDir().set(".");
 
-        getCircleLogFilePath()
-                .fileProvider(getArtifactLocation()
-                        .map(ArtifactLocation::physicalPath)
-                        .map(RegularFile::getAsFile));
+        getCircleLogFilePath().fileProvider(internals.circleLogFile());
 
-        getShowRealTimeLogs().set(isOnCi().map(isOnCi -> !isOnCi));
+        getShowRealTimeLogs().set(internals.isOnCi().map(isOnCi -> !isOnCi));
         getCheckExitStatus().set(true);
         getMaxRetries().set(getProject().provider(() -> retryWhen.isEmpty() ? 1 : 5));
     }
 
     @TaskAction
     public final void exec() {
-        WorkQueue workQueue = getWorkerExecutor().noIsolation();
+        WorkQueue workQueue = getBetterExecInternalWorkerExecutor().noIsolation();
 
         workQueue.submit(BetterExecAction.class, params -> {
             params.getCommand().set(getCommand());
@@ -80,9 +64,8 @@ public abstract class BetterExec extends DefaultTask implements BetterExecCommon
             params.getShouldIncludeStacktraceForFailure().set(getShouldIncludeStacktraceForFailure());
 
             params.getRetryWhen().set(retryWhen);
-            params.getIsOnCi().set(isOnCi());
-            params.getCircleArtifactsUrlLocation()
-                    .set(getArtifactLocation().map(ArtifactLocation::circleLink).orElse(""));
+            params.getIsOnCi().set(internals.isOnCi());
+            params.getCircleArtifactsUrlLocation().set(internals.circleArtifactsUrl());
         });
     }
 
@@ -106,31 +89,5 @@ public abstract class BetterExec extends DefaultTask implements BetterExecCommon
 
     public final void retryWhenOutputContains(String substring) {
         retryWhen(output -> output.contains(substring));
-    }
-
-    private Provider<Boolean> isOnCi() {
-        return getEnvVarsFromUtils()
-                .envVarOrFromTestingProperty("CI")
-                .map(_value -> true)
-                .orElse(false);
-    }
-
-    private Provider<ArtifactLocation> findAvailableLocation(String baseName) {
-        return getCircleCiArtifacts().resolveArtifactLocation(baseName + ".log").map(location -> {
-            if (!location.physicalPath().getAsFile().exists()) {
-                return location;
-            }
-
-            return IntStream.iterate(2, i -> i + 1)
-                    .mapToObj(i -> {
-                        String fileName = baseName + "." + i + ".log";
-                        return getCircleCiArtifacts()
-                                .resolveArtifactLocation(fileName)
-                                .get();
-                    })
-                    .filter(loc -> !loc.physicalPath().getAsFile().exists())
-                    .findFirst()
-                    .get();
-        });
     }
 }
---
+++ better-exec/src/main/java/com/palantir/gradle/betterexec/BetterExecInternals.java
@@ -0,0 +1,81 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.gradle.betterexec;
+
+import com.palantir.gradle.utils.circleciartifacts.ArtifactLocation;
+import com.palantir.gradle.utils.circleciartifacts.CircleCiArtifacts;
+import com.palantir.gradle.utils.environmentvariables.EnvironmentVariables;
+import java.io.File;
+import java.util.stream.IntStream;
+import org.gradle.api.file.RegularFile;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
+
+/**
+ * Encapsulates CircleCI artifact and environment variable logic so that {@link BetterExec}'s bytecode does not
+ * directly reference implementation types like {@link ArtifactLocation}, {@link CircleCiArtifacts}, or
+ * {@link EnvironmentVariables}. This allows those dependencies to be {@code implementation} rather than {@code api},
+ * since {@code validatePlugins} can load {@link BetterExec} without needing those types on the compile classpath.
+ *
+ * @see <a href="https://github.com/gradle/gradle/issues/37091">gradle/gradle#37091</a>
+ */
+final class BetterExecInternals {
+
+    private final CircleCiArtifacts circleCiArtifacts;
+    private final EnvironmentVariables environmentVariables;
+    private final Property<ArtifactLocation> artifactLocation;
+
+    BetterExecInternals(ObjectFactory objectFactory, String baseName) {
+        circleCiArtifacts = objectFactory.newInstance(CircleCiArtifacts.class);
+        environmentVariables = objectFactory.newInstance(EnvironmentVariables.class);
+        artifactLocation = objectFactory.property(ArtifactLocation.class);
+
+        artifactLocation.set(findAvailableLocation(baseName));
+        artifactLocation.finalizeValueOnRead();
+    }
+
+    Provider<File> circleLogFile() {
+        return artifactLocation.map(ArtifactLocation::physicalPath).map(RegularFile::getAsFile);
+    }
+
+    Provider<Boolean> isOnCi() {
+        return environmentVariables.isCi();
+    }
+
+    Provider<String> circleArtifactsUrl() {
+        return artifactLocation.map(ArtifactLocation::circleLink).orElse("");
+    }
+
+    private Provider<ArtifactLocation> findAvailableLocation(String baseName) {
+        return circleCiArtifacts.resolveArtifactLocation(baseName + ".log").map(location -> {
+            if (!location.physicalPath().getAsFile().exists()) {
+                return location;
+            }
+
+            return IntStream.iterate(2, i -> i + 1)
+                    .mapToObj(i -> {
+                        String fileName = baseName + "." + i + ".log";
+                        return circleCiArtifacts
+                                .resolveArtifactLocation(fileName)
+                                .get();
+                    })
+                    .filter(loc -> !loc.physicalPath().getAsFile().exists())
+                    .findFirst()
+                    .get();
+        });
+    }
+}
